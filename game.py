ec360254d83f037fa84b2c37c057f5a8ff361b34
Sundeep-kp <sundeep25101@iiitnr.edu.in>
2025-09-07 22:30:51 +0530

    added multiplatform support (windows, mac and linux). added a new ridiculously hard bonus level

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+
 import time
 import os
 import random
@@ -7,6 +8,15 @@ print("I'll now show you a random number, remember it.")
 time.sleep(1)
 print("if you successfully rewrite it, then you win, else prepare for a consequence")
 input("press enter to start")
+os_platform = sys.platform
+if os_platform.startswith('win'):
+    print("Running on Windows.")
+elif os_platform == 'linux':
+    print("Running on Linux.")
+elif os_platform == 'darwin':
+    print("Running on macOS.")
+else:
+    print("Running on an unknown OS.Your OS may not be supported")
 class level:
   def __init__(self,delay):
     self.delay=delay
@@ -14,14 +24,20 @@ class level:
     random_int = random.randint(11111,1111111)
     print(random_int)
     time.sleep(self.delay)
-    os.system("clear")
+    if os_platform.startswith('win'):
+        os.system("cls")
+    else:
+        os.system("clear")
+     
+
     answer=int(input("what was the number? \n"))
     if answer == random_int:
       print("hurray")
       time.sleep(2)
-    else: print("fuck youuuuuu")
-    sys.exit()
-    end
+    else: 
+        print("fuck youuuuuu")
+        sys.exit()
+        
 print("level 1")
 time.sleep(2)
 level(2).play()
@@ -30,5 +46,18 @@ time.sleep(2)
 level(1).play()
 print("level 3")
 time.sleep(2)
-level(0.4).play()
+level(1).play()
+
+print("you have finished all rounds, congrats..\n")
+time.sleep(1.5)
+print("would you like to continue to a bonus round? :) (Y/N)")
+answer2=input("")
+if answer2 == "Y" or "y" or "yes" or "yup" or "yeah":
+   print("hehehehe")
+   level(0.150).play()
+else:
+   sys.exit()
+
+
+
 
